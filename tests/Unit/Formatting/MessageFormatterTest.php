d9611c8a1fb6f516e265306a0e5092b47fe068aa
Marek Bartoš <bartos.developer152@gmail.com>
2021-01-25 23:47:02 +0100

    MessageFormatter: test more messages, reformat existing

--- tests/Unit/Formatting/MessageFormatterTest.php
+++ tests/Unit/Formatting/MessageFormatterTest.php
@@ -63,11 +63,11 @@ final class MessageFormatterTest extends TestCase
 	{
 		yield ['en-US', 'I have {0} apples.', [3], 'I have 3 apples.'];
 		yield ['en-US', 'I have {0, number, integer} apples.', [3], 'I have 3 apples.'];
-		yield ['en_US', 'I have {number_apples, number, integer} apples.', ['number_apples' => 3], 'I have 3 apples.'];
+		yield ['en_US', 'I have {apples, number, integer} apples.', ['apples' => 3], 'I have 3 apples.'];
 
 		// TODO - another languages
-		//yield ['ar', 'I have {number_apples, number, integer} apples.', ['number_apples' => 3], 'I have ٣ apples.'];
-		//yield ['br', 'I have {number_apples, number, integer} apples.', ['number_apples' => 3], 'I have ৩ apples.'];
+		//yield ['ar', 'I have {apples, number, integer} apples.', ['apples' => 3], 'I have ٣ apples.'];
+		//yield ['br', 'I have {apples, number, integer} apples.', ['apples' => 3], 'I have ৩ apples.'];
 	}
 
 	/**
@@ -102,9 +102,58 @@ final class MessageFormatterTest extends TestCase
 	 */
 	public function providePlural(): Generator
 	{
-		yield ['en-US', 'I have {number_apples, plural, =0{no apples} =1{one apple} other{# apples}}', ['number_apples' => 0], 'I have no apples'];
-		yield ['en-US', 'I have {number_apples, plural, =0{no apples} =1{one apple} other{# apples}}', ['number_apples' => 1], 'I have one apple'];
-		yield ['en-US', 'I have {number_apples, plural, =0{no apples} =1{one apple} other{# apples}}', ['number_apples' => 10], 'I have 10 apples'];
+		$message1 = 'I have {apples, plural, =0{no apples} =1{one apple} other{# apples}}';
+
+		yield ['en-US', $message1, ['apples' => 0], 'I have no apples'];
+		yield ['en-US', $message1, ['apples' => 1], 'I have one apple'];
+		yield ['en-US', $message1, ['apples' => 10], 'I have 10 apples'];
+
+		$message2 = <<<'MSG'
+{apples, plural,
+	=0    {I have no apples}
+	one   {I have one apple}
+	other {I have # apples}
+}
+MSG;
+
+		yield ['en-US', $message2, ['apples' => 0], 'I have no apples'];
+		yield ['en-US', $message2, ['apples' => 1], 'I have one apple'];
+		yield ['en-US', $message2, ['apples' => 10], 'I have 10 apples'];
+
+		$message3 = <<<'MSG'
+I have {apples, plural,
+	=0    {no apples}
+	one   {one apple}
+	other {# apples}
+}
+MSG;
+
+		yield ['en-US', $message3, ['apples' => 0], 'I have no apples'];
+		yield ['en-US', $message3, ['apples' => 1], 'I have one apple'];
+		yield ['en-US', $message3, ['apples' => 10], 'I have 10 apples'];
+
+		// plural by language rules - zero, one, two, few, many, other
+		// Note: this method understands rules of language and ignore cases which are not used by given language
+		$message4 = 'I have {apples, plural, zero{no apples} one{one apple} two{two apples} few{# apples (few)} many{# apples (many)} other{# apples (other)}}';
+
+		yield ['en-US', $message4, ['apples' => 0], 'I have 0 apples (other)'];
+		yield ['en-US', $message4, ['apples' => 1], 'I have one apple'];
+		yield ['en-US', $message4, ['apples' => 2], 'I have 2 apples (other)'];
+		yield ['en-US', $message4, ['apples' => 3], 'I have 3 apples (other)'];
+		yield ['en-US', $message4, ['apples' => 999], 'I have 999 apples (other)'];
+
+		// TODO - symfony and ext-intl have different result
+		//yield ['en-US', $message4, ['apples' => 9999], 'I have 9,999 apples (other)'];
+
+		$message5 = 'Já mám {apples, plural, zero{žádná jablka} one{jedno jablko} two{dvě jablka} few{# jablka (few)} many{# jablek (many)} other{# jablek (other)}}';
+
+		yield ['cs-CZ', $message5, ['apples' => 0], 'Já mám 0 jablek (other)'];
+		yield ['cs-CZ', $message5, ['apples' => 1], 'Já mám jedno jablko'];
+
+		// TODO - symfony chooses other instead of few
+		//yield ['cs-CZ', $message5, ['apples' => 2], 'Já mám 2 jablka (few)'];
+		//yield ['cs-CZ', $message5, ['apples' => 3], 'Já mám 3 jablka (few)'];
+		yield ['cs-CZ', $message5, ['apples' => 999], 'Já mám 999 jablek (other)'];
 	}
 
 	/**
@@ -112,18 +161,21 @@ final class MessageFormatterTest extends TestCase
 	 */
 	public function provideChoice(): Generator
 	{
-		yield [
-			'en-US',
-			'The value of {0,number} is {0, choice,
+		$message1 = <<<'MSG'
+The value of {0,number} is {0, choice,
 				 	0 #between 0 and 19|
 				 	20 #between 20 and 39|
 				 	40 #between 40 and 59|
 				 	60 #between 60 and 79|
 				 	80 #between 80 and 100|
-				 	100 <more than 100}',
-			[60],
-			'The value of 60 is between 60 and 79',
-		];
+				 	100 <more than 100}
+MSG;
+
+		yield ['en-US', $message1, [60], 'The value of 60 is between 60 and 79'];
+		yield ['en-US', $message1, [85], 'The value of 85 is between 80 and 100'];
+
+		// Out of range choose the closest
+		yield ['en-US', $message1, [-30], 'The value of -30 is between 0 and 19'];
 	}
 
 	/**
@@ -145,25 +197,28 @@ final class MessageFormatterTest extends TestCase
 	public function provideComplex(): Generator
 	{
 		$complexPattern
-			= '{gender_of_host, select, ' .
-			'female {' .
-			'{num_guests, plural, offset:1 ' .
-			'=0 {{host} does not give a party.}' .
-			'=1 {{host} invites {guest} to her party.}' .
-			'=2 {{host} invites {guest} and one other person to her party.}' .
-			'other {{host} invites {guest} and # other people to her party.}}}' .
-			'male {' .
-			'{num_guests, plural, offset:1 ' .
-			'=0 {{host} does not give a party.}' .
-			'=1 {{host} invites {guest} to his party.}' .
-			'=2 {{host} invites {guest} and one other person to his party.}' .
-			'other {{host} invites {guest} and # other people to his party.}}}' .
-			'other {' .
-			'{num_guests, plural, offset:1 ' .
-			'=0 {{host} does not give a party.}' .
-			'=1 {{host} invites {guest} to their party.}' .
-			'=2 {{host} invites {guest} and one other person to their party.}' .
-			'other {{host} invites {guest} and # other people to their party.}}}}';
+			= <<<'MSG'
+{gender_of_host, select,
+    female {{num_guests, plural, offset:1
+        =0    {{host} does not give a party.}
+        =1    {{host} invites {guest} to her party.}
+        =2    {{host} invites {guest} and one other person to her party.}
+        other {{host} invites {guest} and # other people to her party.}
+    }}
+    male {{num_guests, plural, offset:1
+        =0    {{host} does not give a party.}
+        =1    {{host} invites {guest} to his party.}
+        =2    {{host} invites {guest} and one other person to his party.}
+        other {{host} invites {guest} and # other people to his party.}
+    }}
+    other {{num_guests, plural, offset:1
+        =0    {{host} does not give a party.}
+        =1    {{host} invites {guest} to their party.}
+        =2    {{host} invites {guest} and one other person to their party.}
+        other {{host} invites {guest} and # other people to their party.}
+    }}
+}
+MSG;
 
 		yield [
 			'en-US',
