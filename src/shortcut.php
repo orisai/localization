6c9579ea668fdcd99177f479f1dd8759a4a50900
Marek Bartoš <bartos.developer152@gmail.com>
2022-07-18 13:10:48 +0200

    tm() shortcut function for object translation

--- src/shortcut.php
+++ src/shortcut.php
@@ -21,3 +21,22 @@ if (!function_exists('Orisai\Localization\t')) {
 	}
 
 }
+
+if (!function_exists('Orisai\Localization\tm')) {
+
+	function tm(TranslatableMessage $message, ?string $languageTag = null): string
+	{
+		static $translator = null;
+
+		if ($translator === null) {
+			$translator = TranslatorHolder::getTranslator();
+		}
+
+		return $translator->translate(
+			$message->getMessage(),
+			$message->getParameters(),
+			$languageTag ?? $message->getLanguageTag(),
+		);
+	}
+
+}
--- tests/Unit/ShortcutTest.php
+++ tests/Unit/ShortcutTest.php
@@ -8,12 +8,14 @@ use Orisai\Localization\Formatting\IntlMessageFormatter;
 use Orisai\Localization\Locale\LocaleProcessor;
 use Orisai\Localization\Locale\Locales;
 use Orisai\Localization\Logging\TranslationsLogger;
+use Orisai\Localization\TranslatableMessage;
 use Orisai\Localization\TranslatorHolder;
 use PHPUnit\Framework\TestCase;
 use Tests\Orisai\Localization\Doubles\ArrayCatalogue;
 use Tests\Orisai\Localization\Doubles\FakeLocaleResolver;
 use Tests\Orisai\Localization\Doubles\SimpleTranslatorGetter;
 use function Orisai\Localization\t;
+use function Orisai\Localization\tm;
 
 /**
  * @runTestsInSeparateProcesses
@@ -59,8 +61,15 @@ final class ShortcutTest extends TestCase
 		TranslatorHolder::setTranslatorGetter(new SimpleTranslatorGetter($translator));
 
 		self::assertSame('No parameters', t('no-param'));
+		self::assertSame('No parameters', tm(new TranslatableMessage('no-param')));
+
 		self::assertSame('I have 3 apples.', t('apples', ['apples' => 3]));
+		self::assertSame('I have 3 apples.', tm(new TranslatableMessage('apples', ['apples' => 3])));
+
 		self::assertSame('Já mám 3 jablka.', t('apples', ['apples' => 3], 'cs'));
+		self::assertSame('Já mám 3 jablka.', tm(new TranslatableMessage('apples', ['apples' => 3], 'cs')));
+		self::assertSame('Já mám 3 jablka.', tm(new TranslatableMessage('apples', ['apples' => 3]), 'cs'));
+		self::assertSame('Já mám 3 jablka.', tm(new TranslatableMessage('apples', ['apples' => 3], 'en'), 'cs'));
 	}
 
 }
